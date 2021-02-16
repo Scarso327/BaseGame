bd4cae22027f4155c0bb01de93da0a50e5ff31ce
Jack Farhall <jackfarhall@gmail.com>
2021-02-16 16:26:06 +0000

    Added : Store for Game object / Removed : IsReady and repalced with null check

--- GameBase/Assets/Scripts/Game/Main/Game.cs
+++ GameBase/Assets/Scripts/Game/Main/Game.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class Game : MonoBehaviour
 {
-    public static bool IsReady = false;
+    public static Game GameObj { private set; get; }
 
     public readonly List<string> CommandArgs = new List<string>(Environment.GetCommandLineArgs());
 
@@ -13,19 +13,19 @@ public class Game : MonoBehaviour
 
     private void Awake()
     {
-        if (IsReady)
+        if (GameObj != null)
             Destroy();
 
         DontDestroyOnLoad(this);
 
         Console = new Console(Instantiate(Resources.Load<GameObject>("Interface/Console")).GetComponent<IConsoleGUI>());
 
-        IsReady = true;
+        GameObj = this;
     }
 
     private void Update()
     {
-        if (!IsReady)
+        if (GameObj != null)
             return;
 
         Console.Update();
@@ -33,7 +33,7 @@ public class Game : MonoBehaviour
 
     private void OnDestroy()
     {
-        IsReady = false;
+        GameObj = null;
     }
 
     private void Destroy()
